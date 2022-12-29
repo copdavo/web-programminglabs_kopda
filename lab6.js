5c654288c343525d734d1faf245e30062b20bfab
kopda <kopylov.vst@gmail.com>
2022-12-29 20:32:11 +0700

    Лесенка

--- lab6.js
+++ lab6.js
@@ -6,11 +6,11 @@ function setRussian(){
     footer.innerHTML = 'Факультет бизнеса, ФБИ-01, 3 курс, 2022';
 
     document.getElementById('Your-name').innerHTML = 'Ваше имя';
-    document.getElementById('imput-name').placeholder = 'введите имя';
+    document.getElementById('input-name').placeholder = 'введите имя';
     document.getElementById('Your-surname').innerHTML = 'Ваша фамилия';
-    document.getElementById('imput-surname').placeholder = 'введите фамилию';
+    document.getElementById('input-surname').placeholder = 'введите фамилию';
     document.getElementById('Your-age').innerHTML = 'Ваш возраст';
-    document.getElementById('imput-age').placeholder = 'введите возраст';
+    document.getElementById('input-age').placeholder = 'введите возраст';
     document.getElementById('ok-button').value = 'ок';
     document.getElementById('reset-button').value = 'сброс';
 
@@ -23,11 +23,11 @@ function setEnglish(){
     footer.innerHTML = 'Faculty of Business, FBI-01, 3rd year, 2022';
 
     document.getElementById('Your-name').innerHTML = 'Your name';
-    document.getElementById('imput-name').placeholder = 'enter your name';
+    document.getElementById('input-name').placeholder = 'enter your name';
     document.getElementById('Your-surname').innerHTML = 'Your surname';
-    document.getElementById('imput-surname').placeholder = 'enter surname';
+    document.getElementById('input-surname').placeholder = 'enter surname';
     document.getElementById('Your-age').innerHTML = 'Your age';
-    document.getElementById('imput-age').placeholder = 'imput age';
+    document.getElementById('input-age').placeholder = 'imput age';
     document.getElementById('ok-button').value = 'ok';
     document.getElementById('reset-button').value = 'reset';
 }
