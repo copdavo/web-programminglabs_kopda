4d8108974121d1c286ff1d5cabbc2a58a5788052
kopda <kopylov.vst@gmail.com>
2023-01-04 07:04:15 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -3,5 +3,15 @@ function showDate(params){
     let today = new Date();
     out.innerHTML = 'Сегодня: ' + today.toLocaleDateString('ru-RU');
 }
+function showDaysCount(params) {
+    let today = new Date()
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    result.innerHTML = daysCount;
+    daysCount = Math.ceil(daysCount); //округляем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
 
 window.addEventListener('load, showDate');
