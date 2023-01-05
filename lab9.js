26a2b8a80aa82352082983ef35dc9cfb7be38412
kopda <kopylov.vst@gmail.com>
2023-01-05 08:56:27 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -28,3 +28,13 @@ function clickMe2(button){
     else
         button.innerHTML = 'Нажми меня';
 }
+function showCoords(event){
+
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + 
+    (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+    window.addEventListener('DOMContentLoaded', function() {
+        let coords = document.querySelector('#coords');
+        coords.addEventListener('mousemove', showCoords);
+});
