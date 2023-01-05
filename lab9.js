9790aee5d752576495d9f7a2c6b13a557c9968b0
kopda <kopylov.vst@gmail.com>
2023-01-05 08:43:11 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,20 @@ function clickName(button){
     else
         button.innerHTML = 'Виктор';
 }
+function clickMe2(button){
+    if( button.innerHTML == 'Нажми меня'){
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'DarkBlue';
+    }
+    else if( button.innerHTML == 'Ещё раз нажми'){
+        button.innerHTML = 'Ещё';
+        button.style.color = 'Violet';}
+    else if( button.innerHTML == 'Ещё'){
+        button.innerHTML = 'Начать сначала';
+        button.style.color = 'BluePink';}
+    else if( button.innerHTML == 'Начать сначала'){
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'Red';}
+    else
+        button.innerHTML = 'Нажми меня';
+}
