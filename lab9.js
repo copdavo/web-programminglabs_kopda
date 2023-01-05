9c7e328169d549c7d923a95e460ef1a031f2b666
kopda <kopylov.vst@gmail.com>
2023-01-05 08:37:32 +0700

    Кнопка Щелчок-Спасибо

--- lab9.js
+++ lab9.js
@@ -1,7 +1,13 @@
 function clickMe(button){
-        if(buytton.innerHTML == 'Щелчок')
+        if(button.innerHTML == 'Щелчок')
             button.innerHTML = 'Спасибо';
         else
             button.innerHTML = 'Щелчок';
 
+}
+function clickName(button){
+    if(button.innerHTML == 'Виктор')
+        button.innerHTML = 'Копылов';
+    else
+        button.innerHTML = 'Виктор';
 }
